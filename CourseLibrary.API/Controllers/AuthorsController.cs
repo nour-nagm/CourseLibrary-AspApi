1b95bc039773090c8428a2c5eee12b3d716bf593
Nour Nagm <nournagm97@gmail.com>
2021-04-17 23:59:38 +0200

    02. Structuring and Implementing the Outer Facing Contract

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,34 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository repository;
+
+        public AuthorsController(ICourseLibraryRepository repository)
+        {
+            this.repository = repository ?? 
+                throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authors = repository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [Route("{authorId:guid}")]
+        public IActionResult GetAuthors(Guid authorId) 
+        {
+            var author = repository.GetAuthor(authorId);
+
+            return author == null ? NotFound() : Ok(author);
+        }
+    }
+}
